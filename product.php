5a7838eaba35662e42880c1bdc9450ce561a081f
Islam Alaa Eldin <eng.islam291@gmail.com>
2016-05-22 12:11:48 +0300

    Add files via upload

---
+++ product.php
@@ -0,0 +1,44 @@
+Welecome this is Product page
+<a href="?route=home">Home Page</a>
+<br><br><br><br>
+<div class="container">
+<div class="row" >
+
+<div class="col-lg-12" id="product_home">
+	<h1 style="text-align:center;font-family:coda">Products</h1>
+</div>
+<div class="row  " id="product_home">
+
+  <div class="col-sm-6 col-md-4" id="men_footwear">
+    <div class="thumbnail hvr-reveal ">
+      <img src="assets/imgs/13.jpg" alt="men footwear">
+      <div class="caption">
+        <h3>Men Footwear</h3>
+        
+        <p><a href="#" class="btn btn-success " role="button">More</a> </p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail  hvr-grow-shadow">
+      <img src="assets/imgs/13.jpg" alt="...">
+      <div class="caption">
+        <h3>Accessorize</h3>
+       
+        <p><a href="#" class="btn btn-success" role="button">More</a> </p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail hvr-bounce-to-right hvr-grow-shadow">
+      <img src="assets/imgs/13.jpg" alt="men footwear">
+      <div class="caption">
+        <h3>Shoe Care</h3>
+       
+        <p><a href="#" class="btn btn-success" role="button">More</a> </p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
